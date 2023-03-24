7d164f22a65cc74aec3b354ae44ecce9433ac354
hwang-jubin <117631618+hwang-jubin@users.noreply.github.com>
2023-03-24 23:38:58 +0900

    괄호 문자 제거
    가독성 좋은 코드

--- Array/5-2.java
+++ Array/5-2.java
@@ -5,17 +5,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
+
         ArrayList<Character> answer = new ArrayList<>();
         Stack<Character> stack = new Stack<>();
-        // 지금부터 (가 나올 때 까지 출력
-        for (char x : str.toCharArray()) {
-            if (x == '(')
-                stack.push(x);
-            else if (x == ')') {
-                stack.pop();
-            } else if (stack.isEmpty()) {
-                answer.add(x);
 
+        int lenStack = 0;
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+                lenStack++;
+            } else if (x == ')') {
+                stack.pop();
+                lenStack--;
+            } else {
+                if (lenStack == 0)
+                    answer.add(x);
             }
         }
 
@@ -23,5 +27,4 @@ public class Main {
             System.out.print(x);
         }
     }
-
 }
