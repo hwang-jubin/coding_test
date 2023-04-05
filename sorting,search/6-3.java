a29ef806779b5d7c3cf2161a0116da30fe5904c1
황주빈 <hjb06288@gmail.com>
2023-04-05 13:20:48 +0900

    삽입정렬

---
+++ sorting,search/6-3.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+//삽입정렬
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp)
+                    arr[j + 1] = arr[j];
+                else
+                    break;
+            }
+            arr[j + 1] = tmp;
+        }
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
